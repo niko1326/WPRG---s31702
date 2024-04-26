335023d731cc3575d5dc876fc66075cc637e820b
Nikodem Hirsch <niko1326.hirsch@gmail.com>
2024-04-26 19:49:33 +0200

    zad11,12 lab06

--- LAB06/zad11.php
+++ LAB06/zad11.php
@@ -1,1 +1,55 @@
+<head>
+    <style>
+        #container{
+            display: flex;
+            flex-direction: row;
+        }
+    </style>
+</head>
+
+
+<html>
+
 <?php
+    echo "<div id='container'>";
+        echo "<div id='table1'>";
+            echo "<table border='1'>";
+            echo "<tr><th>Celsius</th><th>Farenheit</th></tr>";
+                for ($i = -30; $i <= 100; $i++) {
+                    $f = $i * (9 / 5) + 32;
+                    $j = $f;
+                    echo "<tr>
+                            <td>
+                                $i
+                            </td>
+                            <td>
+                                $j
+                            </td>
+                          </tr>";
+                }
+            echo "</table>";
+        echo "</div>";
+
+        echo "<div id='table2'>";
+            echo "<table border='1'>";
+            echo "<tr><th>Farenheit</th><th>Celsius</th></tr>";
+                for ($i = 120; $i >= -22; $i--) {
+                    $f = $i / (9 / 5) - 32;
+                    $j = $f;
+                    echo "<tr>
+                            <td>
+                                $i
+                            </td>
+                            <td>
+                                $j
+                            </td>
+                          </tr>";
+                }
+            echo "</table>";
+        echo "</div>";
+    echo "</div>";
+
+?>
+
+
+</html>
--- LAB06/zad12.php
+++ LAB06/zad12.php
@@ -1,1 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kształt w zależności od minuty</title>
+    <style>
+        .shape {
+            width: 200px;
+            height: 200px;
+            border-radius: 50%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 24px;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
 <?php
+// Pobierz aktualną minutę
+$current_minute = date('i');
+
+// Wybierz kolor kształtu w zależności od minuty
+$color = '';
+if ($current_minute < 20) {
+    $color = 'green'; // Zielony, jeśli minuta jest mniejsza niż 20
+} elseif ($current_minute < 40) {
+    $color = 'blue'; // Niebieski, jeśli minuta jest mniejsza niż 40
+} else {
+    $color = 'red'; // Czerwony, jeśli minuta jest większa lub równa 40
+}
+
+// Wyświetl kształt z odpowiednim kolorem
+echo "<div class='shape' style='background-color: $color;'>$current_minute</div>";
+?>
+</body>
+</html>
